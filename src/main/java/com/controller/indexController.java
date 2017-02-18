801db6135cbb5f526a186dfbf9c6ad380190e83d
Max Ma <mashuoyi111@sina.com>
2017-02-18 01:20:01 -0800

    bootstrap with login page. mybatis structure done

--- src/main/java/com/controller/indexController.java
+++ src/main/java/com/controller/indexController.java
@@ -2,15 +2,12 @@ package com.controller;
 
 import com.domain.User;
 import com.domain.Website;
-import org.apache.ibatis.io.Resources;
+import com.tools.DBtool;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.Reader;
 import java.util.List;
 
 /**
@@ -19,44 +16,33 @@ import java.util.List;
 
 @Controller
 public class indexController {
-    private static SqlSessionFactory sqlSessionFactory;
-    private static Reader reader;
-
-    static{
-        try{
-            reader    = Resources.getResourceAsReader("mybatis.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public static SqlSessionFactory getSession(){
-        return sqlSessionFactory;
-    }
 
 
     @RequestMapping("/hello")
     public ModelAndView helloWorld() {
-        SqlSession session = sqlSessionFactory.openSession();
+        SqlSession session = DBtool.getSession();
         String message = "Website names are: ";
         try {
-            List<Website> websites = session.selectList("getAllWebsites", 1);
+            List<Website> websites = session.selectList("getAllWebsites",1);
             List<User> users=session.selectList("getAllUsers",1);
             Website we=new Website();
             we.setName("sadwa");
             we.setId(4);
-        //    session.insert("insertWebsite",we);
+            System.out.println(we.getName());
+ //           session.insert("insertWebsite",we);
+ //           session.commit();
             for (Website w:websites) {
                 message=message.concat(w.getName());
             }
             for(User u:users){
                 message=message.concat(u.getUser_name());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             session.close();
         }
-        return new ModelAndView("hellopage", "message", message);
+        return new ModelAndView("login", "login", message);
     }
 
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Website Manager!</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/signin.css" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="signin">
+	<div class="signin-head"><img src="pic/myself.jpg" alt="" class="img-rounded"></div>
+	<form class="form-signin" role="form" name="form" action="index.handle.php" method="post">
+		<input type="text" class="form-control" name="username" id="username" required autofocus />
+		<input type="password" class="form-control" name="password" id="password" required />
+		<button class="btn btn-lg btn-danger btn-block" type="submit">login</button>
+		<label class="checkbox">
+			<div class="remember"><input type="checkbox" value="remember-me"> Remeber me</div>
+		</label>
+	</form>
+	<form class="form-signin" role="form" name="form" action="register.php" method="post">
+		<button class="btn btn-lg btn-success btn-block" type="button">register</button>
+	</form>
+
+</div>
+
+</body>
+</html>
